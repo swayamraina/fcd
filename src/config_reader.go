336be98a20187e847519af676be3752aeb75e900
swayamraina <swayamraina@gmail.com>
2020-03-14 16:41:42 +0530

    added yaml config reader

---
+++ src/config_reader.go
@@ -0,0 +1,27 @@
+/**
+ * @author: swayamraina@gmail.com
+ * @dated : 14.03.2020
+**/
+
+package main
+
+import (
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+)
+
+
+/**
+ * This function reads the config file to create the
+ * daemon config object at startup.
+**/
+func get_config (path string) fcd_config {
+
+	data, err := ioutil.ReadFile(path)
+	if err != nil { panic(err) }
+
+	var config fcd_config
+	yaml.Unmarshal(data, &config)
+
+	return config
+}
