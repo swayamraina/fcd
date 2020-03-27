f846ecd5dafe68039f13e2dc8b563f92e2d5ea40
swayamraina <swayamraina@gmail.com>
2020-03-27 07:20:46 +0530

    updated logic to mark a file as synced

--- src/os_handler.go
+++ src/os_handler.go
@@ -1,15 +1,38 @@
+/**
+ *
+ * @author: swayamraina@gmail.com
+ * @dated : 22.03.2020
+ *
+**/
+
+
 package main
 
+
 import (
-	"fmt"
 	"os"
 	"path/filepath"
 	"strings"
 )
 
-const sync_indicator = "fcd-%"
+
+/**
+ *
+ * This connstant is used to mark the scanned
+ * files as synced.
+ * This is also used to check for new files in
+ * the search locations.
+ *
+**/
+const sync_indicator = "fcd-"
 
 
+/**
+ *
+ * This utility function checks if the incoming
+ * file/dir exists in file system or not
+ *
+**/
 func check_on_os (path string) bool {
 	_, err := os.Stat(path)
 	if err == nil { return true }
@@ -18,14 +41,24 @@ func check_on_os (path string) bool {
 }
 
 
-
+/**
+ *
+ * This utility function creates a dir on the
+ * file system.
+ *
+**/
 func create_on_os (path string) bool {
 	err := os.Mkdir(path, os.ModePerm)
 	return err == nil
 }
 
 
-
+/**
+ *
+ * This function checks if a particular file has
+ * been alreday synced to github or is a new file
+ *
+**/
 func check_sync (files *[]string) filepath.WalkFunc {
 	return func (path string, info os.FileInfo, err error) error {
 		if !info.IsDir() && !strings.HasPrefix(info.Name(), sync_indicator) {
@@ -36,8 +69,16 @@ func check_sync (files *[]string) filepath.WalkFunc {
 }
 
 
-func mark_sync (file string) bool {
-	new_file := fmt.Sprintf(sync_indicator, file)
-	err := os.Rename(file, new_file)
-	return err != nil
+/**
+ *
+ * This function marks a specified file as synced.
+ * fcd adds a marker in the beginning of the filename
+ * to maark the file as synced.
+ *
+**/
+func mark_sync (path string) bool {
+	base := extract_base_name(path)
+	new_filename := base + sync_indicator + extract_file_name(path)
+	err := os.Rename(path, new_filename)
+	return err == nil
 }
