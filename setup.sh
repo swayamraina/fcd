da5c720e0ef3ac6b57b16e52d3f97d0d7a1b5fc8
swayamraina <swayamraina@gmail.com>
2020-03-31 23:44:21 +0530

    setup fcd using setup script

--- setup.sh
+++ setup.sh
@@ -11,3 +11,24 @@ read path_to_fcd_config
 
 # create env file
 echo ${path_to_fcd_config} >> ~/.fcd_env
+
+# create directory
+mkdir -p ~/.fcd
+
+# get latest build
+latest_build="$(curl https://raw.githubusercontent.com/swayamraina/fcd/master/release/latest_build)"
+
+# download latest build
+curl -O https://raw.githubusercontent.com/swayamraina/fcd/master/release/${latest_build}
+
+# mark as executable
+chmod +x ${latest_build}
+
+# move to the new directory
+mv ${latest_build} ~/.fcd/
+
+# move pointer to executable location
+cd ~/.fcd/
+
+# run fcd
+./${latest_build}
